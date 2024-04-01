8febc6f63ae8f5513e6dcbeae8a6e58f3738584b
Padla08 <logohl@mail.ru>
2024-04-01 10:05:06 +0300

    Fixed rectangle calc

--- rectangle.java
+++ rectangle.java
@@ -1,17 +1,21 @@
-package BruhRepos;
-
 public class rectangle {
-	double width;
 	
-	double height;
+    double width;
 	
-	void pOutput(){
-		double perim = 2*(width + height);
-		double plosh = width*height;
-		double diag = Math.sqrt(Math.pow(width, 2) + Math.pow(height,2));
-		
-		System.out.printf("Rectangle perimeter: %.3f\n", perim);
-		System.out.printf("Rectangle area: %.3f\n ", plosh);
-		System.out.printf("Rectangle diagonal: %.3f\n ", diag);
-	}
+    double height;
+
+    void pOutput() {
+        if (width <= 0 || height <= 0) {
+            System.out.println("Bad numbers");
+            return;
+        }
+
+        double perim = 2 * (width + height);
+        double area = width * height;
+        double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+        System.out.printf("Rectangle perimeter: %.3f\n", perim);
+        System.out.printf("Rectangle area: %.3f\n", area);
+        System.out.printf("Rectangle diagonal: %.3f\n", diagonal);
+    }
 }
